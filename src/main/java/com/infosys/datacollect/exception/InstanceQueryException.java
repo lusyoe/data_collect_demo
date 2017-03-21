b382b065dd7f1e42e75f0552574626e6adc0c4c3
lu <lusyoe@163.com>
2017-03-21 22:44:48 +0800

    添加异常处理

---
+++ src/main/java/com/infosys/datacollect/exception/InstanceQueryException.java
@@ -0,0 +1,22 @@
+package com.infosys.datacollect.exception;
+
+public class InstanceQueryException extends RuntimeException {
+
+    public InstanceQueryException() {
+        super();
+    }
+
+    public InstanceQueryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InstanceQueryException(String message) {
+        super(message);
+    }
+
+    public InstanceQueryException(Throwable cause) {
+        super(cause);
+    }
+
+    
+}
