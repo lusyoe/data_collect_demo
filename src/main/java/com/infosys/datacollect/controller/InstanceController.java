6d6c354c3eee35457c409828eafd90817357f166
lu <lusyoe@163.com>
2017-03-22 19:55:11 +0800

    添加类文档显示

--- src/main/java/com/infosys/datacollect/controller/InstanceController.java
+++ src/main/java/com/infosys/datacollect/controller/InstanceController.java
@@ -1,22 +1,5 @@
 package com.infosys.datacollect.controller;
 
-import static com.infosys.datacollect.utils.Constans.OK;
-
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.fastjson.JSON;
 import com.infosys.datacollect.dao.InstanceTypeMapper;
 import com.infosys.datacollect.domain.Instance;
@@ -26,13 +9,23 @@ import com.infosys.datacollect.domain.Message;
 import com.infosys.datacollect.exception.InstanceNotFoundException;
 import com.infosys.datacollect.exception.InstanceQueryException;
 import com.infosys.datacollect.service.InstanceService;
-
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+import static com.infosys.datacollect.utils.Constans.OK;
 
 @RestController
 @RequestMapping("/v1/instance")
+@Api(value = "/v1/instance", tags = "资源管理", description = "资源管理模块接口v1")
 public class InstanceController {
 
     @Autowired
